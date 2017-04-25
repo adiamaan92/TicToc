6a126533e4b54a093f33dddbc441faaca819ff9b
praveenpuvvadi <praveenpuvvadi@ou.edu>
2017-04-25 17:11:46 -0500

    add button to bottom and sample code for calling function through events

--- UI.py
+++ UI.py
@@ -1,4 +1,6 @@
-from Tkinter import *
+import tkinter
+
+from tkinter import *
 
 # Application class
 class Application(Frame):
@@ -7,17 +9,29 @@ class Application(Frame):
 
     def create_widgets(self):
         self.hi_there = Button(self)
-        self.hi_there["text"] = "Simulate!"
-        self.hi_there["command"] = self.say_hi
+        # self.hi_there["text"] = "Simulate!"
+        # self.hi_there["command"] = self.say_hi
+        topFrame = Frame(root)
+        topFrame.pack()
+        bottomFrame = Frame(root)
+        bottomFrame.pack(side=BOTTOM)
+        self.button1 = Button(bottomFrame, text="simulate", fg="black", bg="green")
+        self.button1.pack(side=BOTTOM)
+        # on left clicking simulate execute the function name specified in the parameter
+        # button1.bind("<Button-1", function name)
         self.hi_there.pack()
         self.algo_label = Label(self, text="Algorithms available\n Select one..")
-        self.algo_label.pack(side="top", fill='both', expand=True, padx=4, pady=4)
+        self.algo_label.pack(side="left", fill='both', expand=True, padx=4, pady=4)
+
+        self.c1 = Checkbutton(self, text="TicToc")
+        self.c1.pack(side="left")
+
+
 
         self.Quit = Button(self)
         self.Quit["text"] = "Quit?"
         self.Quit["fg"] = "red"
         self.Quit["command"] = self.quit
-
         self.Quit.place(rely=1.0, relx=1.0, x=0, y=0, anchor=SE)
 
     def __init__(self, master=None):
@@ -36,10 +50,12 @@ def center_window(width=300, height=200):
     y = (screen_height / 2) - (height / 2)
     root.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
-
+#main window
 root = Tk()
 root.title("Concurrency Simulator")
 center_window(600, 500)
 app = Application(master=root)
+#continuously run window
 app.mainloop()
+
 root.destroy()
