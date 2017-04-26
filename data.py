c302c339c48ee3e6d7237150f76b00ef04243a9a
adiamaan92 <mak.adi55@gmail.com>
2017-04-25 18:12:33 -0600

    Making minor changes to TicToc

--- data.py
+++ data.py
@@ -1,8 +1,10 @@
-# All tables in the database should have a releveant datastructure created like this
+# All the tables are created as a class here. It will have two extra fields check_id and change
+# All tables in the database should have a relevant data structure created like this
 # It can have any number of fields,  but check_id and change are the important fields
 # check_id is the primary key in a table and change is the field that you want to change
 # only the check_id can be used to access the records and the change is the only field that
 # can be changed during the process
+
 
 class User(object):
     def __init__(self, user_id, name, age, wts=0, rts=0):
