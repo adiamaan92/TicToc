331985e3ed330bc867efeda1cedb3e47c9c7476f
praveenpuvvadi <praveenpuvvadi@ou.edu>
2017-04-25 19:30:19 -0500

    scrollbar and other minor changes

--- UI.py
+++ UI.py
@@ -10,22 +10,22 @@ class Application(Frame):
         # algorithms grid
         self.algo_label = Label(self, text="Please select one algorithm")
         self.algo_label.grid(row=0, column=0, columnspan=2, sticky=W)
-
         self.c1 = Checkbutton(self, text="TicToc")
         self.c1.grid(row=1, column=0, columnspan=2, sticky=W)
         self.c1 = Checkbutton(self, text="TO")
         self.c1.grid(row=2, column=0, columnspan=2, sticky=W)
-
                #CENTER GRID
         # to read text entered in the text box
         self.usertext = StringVar()
         #transaction entry box
         self.label1 = Label(self, text="Please Enter transaction in the given box" )
         self.label1.grid(row=0, column=2, columnspan=2, sticky=W+E+N+S,)
-
-        self.entrybox = Text(self, width = 35, height = 5, wrap = WORD,)
+        self.entrybox = Text(self, width = 35, height = 10, wrap = WORD)
         self.entrybox.grid(row=1, column=2, columnspan=2, sticky=W+E+N+S)
-
+        #adding scrollbar to the box
+        self.scroll = Scrollbar(self, orient="vertical", command=self.entrybox.yview)
+        self.scroll.grid(row=1,column=4,columnspan=1, sticky=W+E+N+S)
+        self.entrybox['yscrollcommand'] = "scroll.set"
         self.button1 = Button(self, text="simulate", fg="black", bg="green")
         self.button1.grid(row=2, column=2, columnspan=2, sticky=W+E+N+S)
         # on left clicking simulate execute the function name specified in the parameter
@@ -34,7 +34,7 @@ class Application(Frame):
             #RIGHT GRID
         #Benchmarks grid
         self.bench_marks = Label(self, text="Please select one Benchmark tool")
-        self.bench_marks.grid(row=0, column=4, columnspan=2, sticky=E)
+        self.bench_marks.grid(row=0, column=5, columnspan=2, sticky=E)
 
         self.c1 = Checkbutton(self, text="YCSB")
         self.c1.grid(row=1, column=4, columnspan=2, sticky=E)
@@ -67,7 +67,8 @@ def center_window(width=100, height=100):
 #main window
 root = Tk()
 root.title("Concurrency Simulator")
-center_window(650, 200)
+root.configure(background="yellow")
+#center_window(650, 400)
 app = Application(master=root)
 #continuously run window
 app.mainloop()
