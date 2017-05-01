32ae3d171a5233dceb51d3267cac191900ba5cc9
keertibanweer <keerti.banweer@ou.edu>
2017-05-01 18:04:49 -0500

    Updated the scenarios

--- ycsb.py
+++ ycsb.py
@@ -12,7 +12,7 @@ import string
 
 
 no_transactions = 0
-user_list = populate_user(10000, db=True)
+user_list = populate_user(1000, db=False)
 Tran_dict = dict()
 arg_list = dict()
 command_list = list()
@@ -34,7 +34,7 @@ no_of_comm_users = int(math.ceil(no_of_queries * hotspot_queries))
 # x% of the queries.
 comm_users = list()
 for i in range(no_of_comm_users):
-    comm_users.append(myPRNG.randint(0, 10000))
+    comm_users.append(myPRNG.randint(0, 100))
 
 
 # To generate the transaction commands for a
@@ -65,7 +65,7 @@ def create_trans(trans_no, flag):
             else:
                 trans_command[i] += str(trans_no) + '(' + not_comm[myPRNG.randint(0, 18 - no_of_comm_users)] + ')\n'
         random.shuffle(trans_command)
-        # print trans_command
+
         return trans_command
     elif flag == "med_contention":
         no_of_reads = 6
@@ -117,21 +117,24 @@ def create_trans(trans_no, flag):
         # print trans_command
         return trans_command
 
+trans_reqd = 10
+count = 0
+
 
 # evaluate 3 different variations of the workload
 # Below we will evaluate 3 different variations of workload
 def read_only():
     variable_value = {}
-    trans_required = 10
+    global trans_reqd, count
     total_trans_read = []
-    # total_trans_read_to = []
+    total_trans_read_to = []
     flag = "read_queries"
-    for i in range(1, trans_required + 1):
+    for i in range(1, trans_reqd + 1):
         total_trans_read += create_trans(i, flag)
 
     random.shuffle(total_trans_read)
 
-    total_trans_read = ['T = ' + str(trans_required) + '\n'] + total_trans_read
+    total_trans_read = ['T = ' + str(trans_reqd) + '\n'] + total_trans_read
     # print total_trans_read
     for i in range(len(total_trans_read)):
         if '(' in total_trans_read[i]:
@@ -139,27 +142,27 @@ def read_only():
             if variable_value.has_key(variable_name):
                 continue
             else:
-                variable_value[variable_name] = myPRNG.randint(0, 10000)
+                variable_value[variable_name] = myPRNG.randint(0, 100)
     # print variable_value
     for entry in variable_value:
         # print entry, variable_value[entry]
         total_trans_read.insert(1, entry + ' = ' + str(variable_value[entry]) + '\n')
 
-    print ''.join(total_trans_read)
-    total_trans_read_to = [','.join(['T' + str(i + 1) for i in range(trans_required)]) + '\n']\
+    # print ''.join(total_trans_read)
+    total_trans_read_to = [','.join(['T' + str(i + 1) for i in range(trans_reqd)]) + '\n']\
                           + total_trans_read
-    # random.shuffle(total_trans_read_to)
+
     ycsb_test = ''.join(total_trans_read)
     ycsb_test_to = ''.join(total_trans_read_to)
-    print ycsb_test
     print ycsb_test_to
     return ycsb_test, ycsb_test_to
 
 
 def medium_contention():
-    trans_reqd = 10
+    global trans_reqd, count
     variable_value = {}
     total_trans = []
+    total_trans_read_to = []
     flag = "med_contention"
     for i in range(1, trans_reqd + 1):
         total_trans += create_trans(i, flag)
@@ -173,31 +176,32 @@ def medium_contention():
             if variable_value.has_key(variable_name):
                 continue
             else:
-                variable_value[variable_name] = myPRNG.randint(0, 10000)
+                variable_value[variable_name] = myPRNG.randint(0, 100)
     # print variable_value
     for entry in variable_value:
         # print entry, variable_value[entry]
         total_trans.insert(1, entry + ' = ' + str(variable_value[entry]) + '\n')
     length = len(total_trans)
     i = 0
+    count = 0
     # for i in range(len(total_trans)):
     while i < length:
         if 'WRITE' in total_trans[i]:
             breakup = total_trans[i].split('(')[0][5:]
             total_trans.insert(i + 1, 'COMMIT' + breakup + '\n')
             length += 1
+            count += 1
         i += 1
 
     total_trans_read_to = [','.join(['T' + str(i + 1) for i in range(trans_reqd)]) + '\n'] \
                           + total_trans
-    # random.shuffle(total_trans_read_to)
     ycsb_test_to = ''.join(total_trans_read_to)
     ycsb_test = ''.join(total_trans)
     return ycsb_test, ycsb_test_to
 
 
 def high_contention():
-    trans_reqd = 10
+    global trans_reqd, count
     variable_value = {}
     total_trans = []
     flag = "high_contention"
@@ -213,7 +217,7 @@ def high_contention():
             if variable_value.has_key(variable_name):
                 continue
             else:
-                variable_value[variable_name] = myPRNG.randint(0, 10000)
+                variable_value[variable_name] = myPRNG.randint(0, 100)
     # print variable_value
     for entry in variable_value:
         # print entry, variable_value[entry]
@@ -221,71 +225,74 @@ def high_contention():
 
     length = len(total_trans)
     i = 0
+    count = 0
     # for i in range(len(total_trans)):
     while i < length:
         if 'WRITE' in total_trans[i]:
             breakup = total_trans[i].split('(')[0][5:]
             total_trans.insert(i + 1, 'COMMIT' + breakup + '\n')
             length += 1
+            count += 1
         i += 1
     total_trans_read_to = [','.join(['T' + str(i + 1) for i in range(trans_reqd)]) + '\n'] \
                           + total_trans
-    # random.shuffle(total_trans_read_to)
     ycsb_test_to = ''.join(total_trans_read_to)
     ycsb_test = ''.join(total_trans)
+    print ycsb_test
     return ycsb_test, ycsb_test_to
-
-set_time = 0
 
 
 def initiate_transaction(trx_list):
     global set_time
+    set_time = 0
     for i in trx_list:
         set_time += 1
         key = "T" + str(i)
         Tran_dict[key] = Transaction_TS(set_time, algorithm2)
 
 
-def initiate_to(trx_list, to_var):
-    global set_time
-    set_time = 0
-    if to_var == 1:
-        for i in trx_list:
-            set_time += 1
-            key = "T" + str(i)
-            Tran_dict[key] = Transaction_TS(set_time, algorithm)
+def algo_parser(text, flag):
+    global no_transactions
+    if flag == "tictoc":
+        no_transactions = 0
+        for i in text:
+            if re.match(r'T\s=\s\d+', i):
+                trx_match = re.match(r'T\s=\s(\d+)', i)
+                no_transactions = int(trx_match.group(1))
+            elif re.match(r'.\s=\s\d+', i):
+                arg_match = re.match(r'(.)\s=\s(\d+)', i)
+                arg_list[arg_match.group(1)] = int(arg_match.group(2))
+            elif re.match(r'READ(\d+)\(.\)', i):
+                read_match = re.match(r'READ(\d+)\((.)\)?', i)
+                command_list.append(['R', read_match.group(1), read_match.group(2)])
+            elif re.match(r'WRITE(\d+)\(.\)', i):
+                write_match = re.match(r'WRITE(\d+)\((.)\)?', i)
+                command_list.append(['W', write_match.group(1), write_match.group(2)])
+            elif re.match(r'COMMIT(\d+)', i):
+                commit_match = re.match(r'COMMIT(\d+)', i)
+                command_list.append(['C', commit_match.group(1), None])
+    else:
+        no_transactions = 0
+        for i in text:
+            if re.match(r'^T\d+,', i):
+                trx_list = re.findall(r'T(\d+)', i)
+                initiate_transaction(trx_list)
+            elif re.match(r'T\s=\s\d+', i):
+                trx_match = re.match(r'T\s=\s(\d+)', i)
+                no_transactions = int(trx_match.group(1))
+            elif re.match(r'.\s=\s\d+', i):
+                arg_match = re.match(r'(.)\s=\s(\d+)', i)
+                arg_list[arg_match.group(1)] = int(arg_match.group(2))
+            elif re.match(r'READ(\d+)\(.\)', i):
+                read_match = re.match(r'READ(\d+)\((.)\)?', i)
+                command_list.append(['R', read_match.group(1), read_match.group(2)])
+            elif re.match(r'WRITE(\d+)\(.\)', i):
+                write_match = re.match(r'WRITE(\d+)\((.)\)?', i)
+                command_list.append(['W', write_match.group(1), write_match.group(2)])
+            elif re.match(r'COMMIT(\d+)', i):
+                commit_match = re.match(r'COMMIT(\d+)', i)
+                command_list.append(['C', commit_match.group(1), None])
 
-
-def initiate_tictoc(no_transactions, tictoc_var):
-    if tictoc_var == 1:
-        for i in range(1, no_transactions + 1):
-            key = "T" + str(i)
-            Tran_dict[key] = Transaction_TicToc(copy.deepcopy(user_list))
-
-
-def algo_parser(text, tictoc_var, to_var,):
-    global no_transactions, command_list, arg_list, Tran_dict
-    command_list, arg_list, Tran_dict = list(), dict(), dict()
-    for i in text:
-        if re.match(r'^T\d+,', i):
-            trx_list = re.findall(r'T(\d+)', i)
-            initiate_to(trx_list, to_var)
-        elif re.match(r'T\s=\s\d+', i):
-            trx_match = re.match(r'T\s=\s(\d+)', i)
-            no_transactions = int(trx_match.group(1))
-            initiate_tictoc(no_transactions, tictoc_var)
-        elif re.match(r'^.\s=\s\d+$', i):
-            arg_match = re.match(r'(.)\s=\s(\d+)', i)
-            arg_list[arg_match.group(1)] = int(arg_match.group(2))
-        elif re.match(r'READ(\d+)\(.\)', i):
-            read_match = re.match(r'READ(\d+)\((.)\)?', i)
-            command_list.append(['R', read_match.group(1), read_match.group(2)])
-        elif re.match(r'WRITE(\d+)\(.\)', i):
-            write_match = re.match(r'WRITE(\d+)\((.)\)?', i)
-            command_list.append(['W', write_match.group(1), write_match.group(2)])
-        elif re.match(r'COMMIT(\d+)', i):
-            commit_match = re.match(r'COMMIT(\d+)', i)
-            command_list.append(['C', commit_match.group(1), None])
 
 # store the results of three different variations of workload
 read_only_results, read_only_results_to = read_only()
@@ -297,13 +304,21 @@ high_contention_results, high_contention_results_to = high_contention()
 def test_tictoc1():
     text1 = read_only_results.splitlines()
     flag = "tictoc"
+    abort = 0
     start_time = time.time()
     algo_parser(text1, flag)
     for i in range(1, no_transactions + 1):
         key = "T" + str(i)
         Tran_dict[key] = Transaction_TicToc(copy.deepcopy(user_list))
 
-    algorithm1.control_logic(Tran_dict, arg_list, command_list)
+    algorithm1.control_logic(Tran_dict, arg_list, command_list, detailed=False)
+    for k, v in Tran_dict.items():
+        if v.status == "Aborted":
+            abort += 1
+    print ("Total number of commits: " + '\n')
+    print (count)
+    print ("Total number of aborts: " + '\n')
+    print (str(abort) + '\n')
     print("Time taken to run tictoc with read only statements" + '\n')
     print("--- %s seconds ---" % (time.time() - start_time))
     print ('\n')
@@ -312,14 +327,21 @@ def test_tictoc1():
 def test_tictoc2():
     text2 = medium_contention_results.splitlines()
     flag = "tictoc"
+    abort = 0
     start_time = time.time()
     algo_parser(text2, flag)
     for i in range(1, no_transactions + 1):
         key = "T" + str(i)
         Tran_dict[key] = Transaction_TicToc(copy.deepcopy(user_list))
 
-    algorithm1.control_logic(Tran_dict, arg_list, command_list)
-    print ('\n')
+    algorithm1.control_logic(Tran_dict, arg_list, command_list, detailed=False)
+    for k, v in Tran_dict.items():
+        if v.status == "Aborted":
+            abort += 1
+    print ("Total number of commits: " + '\n')
+    print (count)
+    print ("Total number of aborts: " + '\n')
+    print (str(abort) + '\n')
     print("Time taken to run tictoc with medium contention level: " + '\n')
     print("--- %s seconds ---" % (time.time() - start_time))
     print ('\n')
@@ -328,13 +350,21 @@ def test_tictoc2():
 def test_tictoc3():
     text3 = high_contention_results.splitlines()
     flag = "tictoc"
+    abort = 0
     start_time = time.time()
     algo_parser(text3, flag)
     for i in range(1, no_transactions + 1):
         key = "T" + str(i)
         Tran_dict[key] = Transaction_TicToc(copy.deepcopy(user_list))
 
-    algorithm1.control_logic(Tran_dict, arg_list, command_list)
+    algorithm1.control_logic(Tran_dict, arg_list, command_list, detailed=False)
+    for k, v in Tran_dict.items():
+        if v.status == "Aborted":
+            abort += 1
+    print ("Total number of commits: " + '\n')
+    print (count)
+    print ("Total number of aborts: " + '\n')
+    print (abort)
     print ('\n')
     print("Time taken to run tictoc with high contention level: " + '\n')
     print("--- %s seconds ---" % (time.time() - start_time))
@@ -344,10 +374,18 @@ def test_tictoc3():
 def test_to1():
     text1 = read_only_results_to.splitlines()
     flag = "timestamp"
+    abort = 0
     start_time = time.time()
     algo_parser(text1, flag)
 
-    algorithm2.control_logic(Tran_dict, arg_list, command_list)
+    algorithm2.control_logic(Tran_dict, arg_list, command_list, detailed=False)
+    for k, v in Tran_dict.items():
+        if v.status == "Aborted":
+            abort += 1
+    print ("Total number of commits: " + '\n')
+    print (count)
+    print ("Total number of aborts: " + '\n')
+    print (abort)
     print ('\n')
     print("Time taken to run timestamp with read only statements" + '\n')
     print("--- %s seconds ---" % (time.time() - start_time))
@@ -357,10 +395,18 @@ def test_to1():
 def test_to2():
     text2 = medium_contention_results_to.splitlines()
     flag = "timestamp"
+    abort = 0
     start_time = time.time()
     algo_parser(text2, flag)
 
-    algorithm2.control_logic(Tran_dict, arg_list, command_list)
+    algorithm2.control_logic(Tran_dict, arg_list, command_list, detailed=False)
+    for k, v in Tran_dict.items():
+        if v.status == "Aborted":
+            abort += 1
+    print ("Total number of commits: " + '\n')
+    print (count)
+    print ("Total number of aborts: " + '\n')
+    print (abort)
     print ('\n')
     print("Time taken to run timestamp with medium contention level: " + '\n')
     print("--- %s seconds ---" % (time.time() - start_time))
@@ -370,10 +416,18 @@ def test_to2():
 def test_to3():
     text3 = high_contention_results_to.splitlines()
     flag = "timestamp"
+    abort = 0
     start_time = time.time()
     algo_parser(text3, flag)
 
-    algorithm2.control_logic(Tran_dict, arg_list, command_list)
+    algorithm2.control_logic(Tran_dict, arg_list, command_list, detailed=False)
+    for k, v in Tran_dict.items():
+        if v.status == "Aborted":
+            abort += 1
+    print ("Total number of commits: " + '\n')
+    print (count)
+    print ("Total number of aborts: " + '\n')
+    print (abort)
     print ('\n')
     print("Time taken to run timestamp with high contention level: " + '\n')
     print("--- %s seconds ---" % (time.time() - start_time))
